afe48475f6acc07b1ca70683ff592fd6c4d0aa55
ALIENvsROBOT <gowtham.sridher5@gmail.com>
2022-07-19 16:04:56 +0200

    init website

--- skill/script.js
+++ skill/script.js
@@ -1,81 +1,100 @@
-"use strict";
-$(function() {
-    new Vue({
-        el: "#app",
-        data: {
-            skillDatas: [],
-            ranks: [{
-                name: "NOOB",
-                picUrl: "https://raw.githubusercontent.com/JackCree/skill-tree-01/be94f93a34396d755cdf9072b35c1248da8ce126/img/skills-icones-tie-figther-basic.svg",
-                sentence: 'Welcome aboard ! My mission: <span class="highline">Collect resources and learn skills to improve the ship.</span><br>Good luck, captain !'
-            }, {
-                name: "BEGINNER",
-                picUrl: "https://raw.githubusercontent.com/JackCree/skill-tree-01/be94f93a34396d755cdf9072b35c1248da8ce126/img/skills-icones-tie-fighter-bomber.svg",
-                sentence: '<b>Congrats !</b> I manage to become a <span class="highline">“Front-End Beginner”</span>. Keep looking for the resources to take it to the next level.'
-            }, {
-                name: "DEVELOPER",
-                picUrl: "https://raw.githubusercontent.com/JackCree/skill-tree-01/be94f93a34396d755cdf9072b35c1248da8ce126/img/skills-icones-tie-fighter-interceptor.svg",
-                sentence: '<b>I am making good progress!!</b> Now I am a <span class="highline">“Front-End Developer”</span>. The upgrade program is almost complete. <br>Next Level: Front End Master'
-            }, {
-                name: "MASTER",
-                picUrl: "https://raw.githubusercontent.com/JackCree/skill-tree-01/be94f93a34396d755cdf9072b35c1248da8ce126/img/skills-icones-tie-fighter-advanced.svg",
-                sentence: '<b>Excellent !</b> I became a<span class="highline">“Front-end Master”</span>. But a new galaxy has emerged in the system.<br>Captain, make your choice.'
-            }],
-            currentRank: 1,
-            currentBranch: {}
-        },
-        methods: {
-            /*Return true when the element in the checklist is completed
-            0: false
-            1: true
-            */
-            currentComplete: function(e) {
-                return null == e ? 0 : e.filter(function(e) {
-                    return 1 == e.complete
-                }).length
-            },
-            //Make appear all the elements in the list to validate the skills
-            focusList: function(e) {
-                this.skillDatas.forEach(function(e) {
-                    e.branch.forEach(function(e) {
-                        e.active = !1
-                    })
-                }),
-                e.active = !0,
-                this.currentBranch = e
-            },
-            //Check if all elements in the checklist are completed
-            toggleListComplete: function(e) {
-                e.complete = !e.complete,
-                this.checkBranchComplete()
-            },
-            //Check if all elements in the branch are completed
-            checkBranchComplete: function() {
-                if (null != this.currentBranch.recommend)
-                    return this.currentBranch.recommendComplete = this.currentBranch.recommend.every(function(e) {
-                        return 1 == e.complete
-                    }),
-                    this.currentBranch.recommendComplete && this.checkLevelComplete()
-            },
-            //check if all brach on levels are completed
-            checkLevelComplete: function() {
-                var n = 0;
-                this.skillDatas.forEach(function(e) {
-                    e.levelComplete = e.branch.every(function(e) {
-                        return 1 == e.recommendComplete
-                    }),
-                    n = 1 == e.levelComplete ? ++n : n
-                }),
-                this.currentRank = 0 == n ? n : n - 1
-            }
-        },
-        //Generate the skill tree with the actual data
-        created: function() {
-            var n = this;
-            $.getJSON("https://raw.githubusercontent.com/JackCree/skill-tree-01/main/skill-tree.json", function(e) {
-                n.skillDatas = e,
-                n.currentBranch = n.skillDatas[0].branch[0]
-            })
+const prefix = 'I love working with ';
+const skills = [
+'Robotics',
+'Python',
+'Robot Operating System',
+'Embedded system',
+'C++',
+'Machine learning',
+'Neural Networks',
+'passion & love'].
+map(s => `${s}.`);
+const delay = 1;
+const step = 1;
+const tail = 7;
+const timeout = 75;
+
+
+const p = document.createElement('p');
+document.body.appendChild(p);
+
+const colors = [
+'red',
+'green',
+'blue',
+'yellow',
+'cyan',
+'magenta'];
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+function getRandomChar() {
+  return String.fromCharCode(Math.random() * (127 - 33) + 33);
+}
+function getRandomColoredString(n) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < n; i++) {
+    const char = document.createElement('span');
+    char.textContent = getRandomChar();
+    char.style.color = getRandomColor();
+    fragment.appendChild(char);
+  }
+  return fragment;
+}
+
+const $ = {
+  text: '',
+  prefixP: -tail,
+  skillI: 0,
+  skillP: 0,
+  direction: 'forward',
+  delay,
+  step };
+
+
+function render() {
+  const skill = skills[$.skillI];
+
+  if ($.step) {
+    $.step--;
+  } else {
+    $.step = step;
+    if ($.prefixP < prefix.length) {
+      if ($.prefixP >= 0) {
+        $.text += prefix[$.prefixP];
+      }
+      $.prefixP++;
+    } else {
+      if ($.direction === 'forward') {
+        if ($.skillP < skill.length) {
+          $.text += skill[$.skillP];
+          $.skillP++;
+        } else {
+          if ($.delay) {
+            $.delay--;
+          } else {
+            $.direction = 'backward';
+            $.delay = delay;
+          }
         }
-    })
-});
+      } else {
+        if ($.skillP > 0) {
+          $.text = $.text.slice(0, -1);
+          $.skillP--;
+        } else {
+          $.skillI = ($.skillI + 1) % skills.length;
+          $.direction = 'forward';
+        }
+      }
+    }
+  }
+
+  p.textContent = $.text;
+  p.appendChild(getRandomColoredString(
+  $.prefixP < prefix.length ?
+  Math.min(tail, tail + $.prefixP) :
+  Math.min(tail, skill.length - $.skillP)));
+  setTimeout(render, timeout);
+}
+setTimeout(render, 500);
