1e49f0b060f628aba8e8ffc89bdda53d3c670819
Gowtham <gowtham.sridher5@gmail.com>
2025-04-14 20:49:43 +0200

    fix image

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -10,7 +10,7 @@ const projects = [
     id: 8,
     title: 'BuildsaVeR: A Virtual Reality Experience for Construction Workers safety training',
     description: 'buildsaVeR is designed to supplement safety training in the future and raise awareness of safe working practices. The biggest advantage: mistakes are completely harmless here - we promise!',
-    image: '/Project_images/buildsaVer.jpg',
+    image: '/Project_images/buildsaver.jpg',
     fallbackImage: 'https://placehold.co/600x350/3d4463/ffffff?text=Robot+Navigation',
     technologies: ['XR', 'Unreal Engine', 'Game Development','Virtual Reality'],
     link: 'https://www.linkedin.com/posts/strabag_strabag-workonprogress-virtualreality-activity-7309816291601612800-nlZw?utm_source=share&utm_medium=member_desktop&rcm=ACoAACXQpi8BHxHGigkfDsMcszwOKDuyojVn8oE',
