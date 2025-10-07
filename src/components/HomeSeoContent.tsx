e34a82bc1800bbe74e4331917dbb14eeaebfd497
Gowtham Sridhar <gowtham.sridher5@gmail.com>
2025-10-07 11:05:04 +0200

    Update HomeSeoContent.tsx

--- src/components/HomeSeoContent.tsx
+++ src/components/HomeSeoContent.tsx
@@ -1,7 +1,7 @@
 import EnhancedSEO from '@/components/EnhancedSEO';
 
-const pageTitle = 'Gowtham Sridhar | HCI Researcher & XR Expert';
-const pageDescription = 'Portfolio of Gowtham Sridhar, Junior Scientist at AIT specializing in Human-Computer Interaction, XR applications, robotics, and tangible interfaces.';
+const pageTitle = 'Gowtham Sridhar | HCI Researcher & AI Expert';
+const pageDescription = 'Portfolio of Gowtham Sridhar, Junior Scientist at AIT specializing in Human-Computer Interaction, Applied AI Research, XR applications, robotics, and tangible interfaces.';
 const pageUrl = 'https://www.gowthamsridhar.com';
 
 export default function HomeSeoContent() {
